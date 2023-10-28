e6c2991a47b6989b3b81d76a960fd3de1e7ba353
Spopoi <davidepanarella.ts@gmail.com>
2023-10-28 16:19:26 +0200

    Customer class

---
+++ Customer.py
@@ -0,0 +1,6 @@
+class Customer:
+
+      def __init__(self, index, location, package_demand=1):
+        self.index = index
+        self.location = location
+        self.package_demand = package_demand
--- mTSP-DS.py
+++ mTSP-DS.py
@@ -1,5 +1,6 @@
 import gurobipy as gp
 from gurobipy import GRB
+from Customer import Customer
 
 #Parameters
 n = 10 # customers
@@ -16,10 +17,15 @@ def solve():
     # Variabili di decisione
     tau = model.addVar(vtype=GRB.CONTINUOUS, name="tau")
     
-    for k in range(Kn):
-        
-    x2 = model.addVar(vtype=GRB.INTEGER, name="x2")
+    for k in range(Kn): 
+        x2 = model.addVar(vtype=GRB.INTEGER, name="x2")
+
+def test_Customer_class():
+    customer_prova = Customer(1,2)
+    print(customer_prova.index)
+    print(customer_prova.package_demand)
     
 
 if __name__ == "__main__":
     solve()
+    test_Customer_class()
