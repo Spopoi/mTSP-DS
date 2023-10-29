0c9ba4b307a7a659f345efe48cfd3d1f15952fd1
Spopoi <davidepanarella.ts@gmail.com>
2023-10-29 16:42:19 +0100

    Drone class

---
+++ Drone.py
@@ -0,0 +1,5 @@
+class Drone:
+      def __init__(self, index, drone_station, speed_factor=1.5):
+        self.index = index
+        self.drone_station = drone_station
+        self.speed_factor = speed_factor
