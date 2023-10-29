14b181c95886e2bf040e1f17cf36a488b88c2f32
Spopoi <davidepanarella.ts@gmail.com>
2023-10-29 16:49:40 +0100

    DroneStation class

--- Drone.py
+++ Drone.py
@@ -1,5 +1,6 @@
 class Drone:
-      def __init__(self, index, drone_station, speed_factor=1.5):
+      def __init__(self, index, drone_station, speed_factor=1.5, eps=100):
         self.index = index
         self.drone_station = drone_station
         self.speed_factor = speed_factor
+        self.eps = eps #max drone distance
---
+++ DroneStation.py
@@ -0,0 +1,6 @@
+class DroneStation:
+    def __init__(self, index, location, dn):
+        self.index = index
+        self.location = location
+        self.dn = dn  # drone number
+
