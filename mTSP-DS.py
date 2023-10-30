e11890bf58424ccd2a09ea40689d8f3ae7dd6321
Spopoi <davidepanarella.ts@gmail.com>
2023-10-30 15:09:10 +0100

    Nodes plot

--- mTSP-DS.py
+++ mTSP-DS.py
@@ -5,6 +5,7 @@ from Truck import Truck
 from Drone import Drone
 from DroneStation import DroneStation
 from Location import Location
+import matplotlib.pyplot as plt
 
 # Parameters
 n = 10  # customers
@@ -21,7 +22,7 @@ v = []  # nodes
 def init():
     if custom_setup:
         # starting depot
-        v.append(0)
+        # v.append(Location(0,0))
         # customers:
         v.append(Customer(1, Location(10, 10)))
         v.append(Customer(2, Location(20, 10)))
@@ -31,7 +32,30 @@ def init():
         v.append(DroneStation(1, Location(30, 30), 2))
         v.append(DroneStation(2, Location(-10, 40), 1))
         # Ending depot
-        v.append(1)
+        # v.append(Location(0, 0))
+        plot_nodes(v)
+
+
+def plot_nodes(nodes):
+    x_values = [node.location.x for node in nodes]
+    y_values = [node.location.y for node in nodes]
+
+    # Colori diversi per Customer e DroneStation
+    colors = ['blue' if isinstance(node, Customer) else 'red' for node in nodes]
+
+    plt.scatter(x_values, y_values, color=colors, label="Nodes")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Plot dei Nodi")
+
+    for node in nodes:
+        plt.annotate(str(node.index), (node.location.x, node.location.y), textcoords="offset points", xytext=(0, 10),
+                     ha='center')
+
+    blue_patch = plt.Line2D([0], [0], marker='o', color='w', label='Customer', markerfacecolor='blue', markersize=10)
+    red_patch = plt.Line2D([0], [0], marker='o', color='w', label='Drone Station', markerfacecolor='red', markersize=10)
+    plt.legend(handles=[blue_patch, red_patch])
+    plt.show()
 
 
 def solve():
@@ -52,7 +76,7 @@ def test():
     print(drone2)
     array_droni = [drone1, drone2]
 
-    station = DroneStation(index=1, location="Station A", drones=array_droni)
+    # station = DroneStation(index=1, location="Station A", drones=array_droni)
     print(station)
 
 
@@ -60,6 +84,6 @@ if __name__ == "__main__":
     solve()
     # test()
     init()
-    for i in range(len(v)):
-        print(str(v[i]) + " ")
+    # for i in range(len(v)):
+    #     print(str(v[i]) + " ")
 
