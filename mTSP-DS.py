ef374aecca603503df2951e07c39771a9683ed64
Spopoi <davidepanarella.ts@gmail.com>
2024-03-08 15:37:39 +0100

    update: Decision variables and objective function

--- mTSP-DS.py
+++ mTSP-DS.py
@@ -8,38 +8,6 @@ from Location import Location
 from Depot import Depot
 import matplotlib.pyplot as plt
 
-# Parameters
-n = 4  # customers
-m = 2  # drone stations
-Dn = 1  # num of drones per drone station
-Kn = 1  # trucks
-C = 1  # max number of actionable drone stations (<= m)
-alpha = 0.5  # drone velocity factor relative to truck speed
-eps = 100  # max drone distance
-custom_setup = True
-
-v = []  # nodes
-k = []  # trucks
-
-
-def init():
-    if custom_setup:
-        # starting depot
-        v.append(Depot(0, Location(0, 0)))
-        # customers:
-        v.append(Customer(1, Location(10, 10)))
-        v.append(Customer(2, Location(20, 10)))
-        v.append(Customer(3, Location(40, 80)))
-        v.append(Customer(4, Location(100, 60)))
-        # Drone stations:
-        v.append(DroneStation(1, Location(10, 30), Dn))
-        v.append(DroneStation(2, Location(60, 40), Dn))
-        # Ending depot
-        v.append(Depot(1, Location(0, 0)))
-        plot_nodes(v)
-        for i in range(Kn):
-            k.append(Truck(i, Location(0, 0)))
-
 
 def plot_nodes(nodes):
     # Todo: Spezzare i nodi e plottarli separatamente
@@ -63,9 +31,65 @@ def plot_nodes(nodes):
     plt.show()
 
 
+def solve():
+    # Parameters
+    n = 4  # customers
+    m = 2  # drone stations
+    Dn = 1  # num of drones per drone station
+    Kn = 2  # trucks
+    C = 1  # max number of actionable drone stations (<= m)
+    alpha = 0.5  # drone velocity factor relative to truck speed
+    eps = 100  # max drone distance
+    custom_setup = True
+
+    v = []  # nodes
+    k = []  # trucks
+
+    if custom_setup:
+        # starting depot
+        v.append(Depot(0, Location(0, 0)))
+        # customers:
+        v.append(Customer(1, Location(10, 10)))
+        v.append(Customer(2, Location(20, 10)))
+        v.append(Customer(3, Location(40, 80)))
+        v.append(Customer(4, Location(100, 60)))
+        # Drone stations:
+        v.append(DroneStation(1, Location(10, 30), Dn))
+        v.append(DroneStation(2, Location(60, 40), Dn))
+        # Ending depot
+        v.append(Depot(1, Location(0, 0)))
+        plot_nodes(v)
+        for i in range(Kn):
+            k.append(Truck(i, Location(0, 0)))
+    vl = v[1:]  # nodes \ initialDepot
+    vr = v[:-1]  # node \ endingDepot
+
+    model = gp.Model("mTSP-DS")
+
+    # Decision variables
+    # Makespan
+    tau = model.addVar(vtype=GRB.CONTINUOUS, name="tau")
+
+    # DS activation
+    z = model.addVars(m, vtype=GRB.INTEGER, name="z")
+
+    # truck k traverse edge (i,j)
+    x_k_ij = model.addVars([(k, i, j) for k in range(Kn) for i in range(n-1) for j in range(n-1)],
+                           vtype=GRB.BINARY, name="x_k_ij")
+
+    # time truck k arrives at node i
+    a_ki = model.addVars([(i, j) for i in range(Kn) for j in range(len(v))], vtype=GRB.CONTINUOUS, name="a_ki")
+
+    # customer j served by drone d from drone station s
+    y_d_sj = model.addVars([(d, s, j) for d in range(Dn) for s in range(m) for j in range(n)],
+                           vtype=GRB.BINARY, name="y_d_sj")
+
+    # model.update()
+    model.setObjective(tau, sense=GRB.MINIMIZE)
+
+    model.update()
+    model.write("modello.lp")
+
+
 if __name__ == "__main__":
-    # solve()
-    # test()
-    init()
-    # for i in range(len(v)):
-    #     print(str(v[i]) + " ")
+    solve()
