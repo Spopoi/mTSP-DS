e424669e402ac4a550124345e1e2a8f49e478c26
Spopoi <davidepanarella.ts@gmail.com>
2024-04-17 14:17:36 +0200

    update: Matheuristic index refactoring and implementation of constraint 18 and 19

--- Local_DASP.py
+++ Local_DASP.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 import numpy as np
 
 from Node import NodeType
@@ -21,8 +23,8 @@ class Local_DASP:
         self.O = []
         self.V_start = self.get_starter_nodes()
         self.Vn = self.get_ds_customers()
-        self.Vn_index = [node.index for node in self.Vn]
-        print("Nodes: ", self.Vn_index)
+        # self.Vn_index = [node.index for node in self.Vn]
+        print("Vn = ", self.Vn)
         self.V_end = self.get_end_nodes()
         self.get_outlier_nodes()
 
@@ -48,18 +50,47 @@ class Local_DASP:
         self.V.extend(self.V_OE)
         self.V.extend(self.V_end)
 
-        # TODO: repetitions allowed?
         # self.V_index = [node.index for node in self.V]
         # self.Vl_index = [node.index for node in self.Vl]
         # #print("Vl_indexes: ", self.Vl_index)
         # self.Vr_index = [node.index for node in self.Vr]
 
         self.V_index = range(len(self.V))
-        self.Vl_index = range(len(self.Vl))
-        print("Vl_indexes: ", self.Vl_index)
-        self.Vr_index = range(len(self.Vr))
-        self.K = range(1, len(self.dasp_tours)+1)
+        print("V_indexes: ", self.V_index)
+        print("V: ", self.V)
+
+        # self.Vl_index = range(len(self.Vl))
+        # print("Vl_indexes: ", self.Vl_index)
+        # self.Vr_index = range(len(self.Vr))
+        # self.K = range(1, len(self.dasp_tours)+1)
+        self.K = range(1, len(self.dasp_tours) + 1)
         self.k = len(self.dasp_tours)
+
+        self.Vn_index = self.V_index[self.k: self.k + len(self.Vn)]
+        print("Vn_index = ", self.Vn_index)
+
+        vl_index = self.k + len(self.Vn) + 1
+        self.Vl_index = self.V_index[:vl_index]
+        self.Vl_index = list(chain(self.Vl_index, self.V_index[vl_index + len(self.V_OS): vl_index + 2 * len(self.V_OS)]))
+        print("Vl_indexes: ", self.Vl_index)
+
+        vr_index = vl_index + len(self.V_OS)
+        self.Vr_index = self.V_index[self.k: vr_index]
+        self.Vr_index = list(
+            chain(self.Vr_index, self.V_index[vr_index+len(self.V_OE):]))
+        print("Vr_indexes: ", self.Vr_index)
+
+        # self.O_index = [(i, j) for i in self.V_index[vl_index:vl_index+len(self.V_OS)]
+        # for j in self.V_index[vr_index:vr_index+len(self.V_OE)]]
+
+        self.V_OS_index = self.V_index[vl_index:vl_index+len(self.V_OS)]
+        self.V_OE_index = self.V_index[vr_index:vr_index+len(self.V_OE)]
+
+        self.O_index = list(zip(self.V_OS_index, self.V_OE_index))
+        print("O_index: ", self.O_index)
+        # self.k = len(self.dasp_tours)
+
+        self.V_end_index = self.V_index[-self.k:]
 
         self.model = None
         self.tau_tilde = None
@@ -78,14 +109,6 @@ class Local_DASP:
             # for each node
             for j in range(1, len(self.tours[i])):
                 node = self.tours[i][j]
-                # print((i, j), node)
-                # print(f"Station= {d_station} location = {self.v[d_station].location}")
-                # print(f"Node {node.index} Location = {node.location}")
-                # print("distance=", node.node_distance(self.v[d_station]))
-                # print("eps=", self.eps / 2)
-                # print("RESULT= ", node.node_distance(self.v[d_station]) <= self.eps / 2)
-                # if node.node_type == NodeType.CUSTOMER and node.node_distance(self.milp_model.v[self.d_station])
-                # <= self.milp_model.eps / 2:
                 # TODO: check, is considered also the case in which the only node in range of ds is ds itself
                 if node.node_distance(self.milp_model.v[self.d_station]) <= self.milp_model.eps / 2:
                     # starter_nodes.append((i, self.tours[i][j - 1]))
@@ -169,11 +192,10 @@ class Local_DASP:
         self.tau_tilde = self.model.addVar(vtype=GRB.CONTINUOUS, name="tau_tilde")
 
         # truck k traverse edge (i,j)
-        # TODO: add (os,oe) pairs
         self.x_k_ij = self.model.addVars([(k, i, j) for k in self.K for i in self.Vl_index
                                           for j in self.Vr_index], vtype=GRB.BINARY, name="x_k_ij")
 
-        self.x_k_ij_outliers = self.model.addVars([(k, i, j) for k in self.K for (i, j) in self.O],
+        self.x_k_ij_outliers = self.model.addVars([(k, i, j) for k in self.K for (i, j) in self.O_index],
                                                   vtype=GRB.BINARY, name="x_k_ij_outliers")
 
         # time truck k arrives at node i
@@ -189,14 +211,30 @@ class Local_DASP:
         # CONSTRAINTS
         # Constraint (16)
         end_k = self.k + len(self.Vn) + 2*len(self.O)
-        print(self.V)
+        print("V = ", self.V)
         self.model.addConstrs((self.a_ki[k, end_k + k] <= self.tau_tilde for k in self.K), name="(16)")
 
         # Constraint (17)
         ds_dasp_index = self.k + len(self.Vn)
         self.model.addConstrs((self.a_ki[k, ds_dasp_index] + gp.quicksum(
-            2 * self.milp_model.t_ij_drone[self.d_station, j] * self.y_d_j[d, j] for j in self.Vn_index) <= self.tau_tilde
-                               for k in self.K for d in self.milp_model.D), name="(17)")
+            2 * self.milp_model.t_ij_drone[self.d_station, self.V[j].index] * self.y_d_j[d, j] for j in self.Vn_index)
+                               <= self.tau_tilde for k in self.K for d in self.milp_model.D), name="(17)")
+
+        # Constraint (18)
+        self.model.addConstrs((gp.quicksum(gp.quicksum(self.x_k_ij[k, i, j] for i in self.Vl_index if i != j)
+                                           for k in self.K) + gp.quicksum(self.y_d_j[d, j] for d in self.milp_model.D)
+                               == 1 for j in self.Vn_index), name="(18)")
+
+        # Constraint (19.1)
+        second_step_node_indexes = list(chain(self.Vn_index, [ds_dasp_index], self.V_OS_index, self.V_end_index))
+        print("Second_step: ", second_step_node_indexes)
+
+        self.model.addConstrs((gp.quicksum(self.x_k_ij[k, k-1, j] for j in second_step_node_indexes) == 1
+                               for k in self.K), name="(19.1)")
+
+        # Constraint (19.2)
+        # self.model.addConstrs(
+        #     (gp.quicksum(self.x_k_ij[k, i, 1 + self.n + self.m] for i in self.Vn_index) == 1 for k in self.K), name="(19.2)")
 
         self.model.update()
         self.model.write("modello_matheuristic.lp")
